c26a6ec96349275fc78c7588efc283bc2883415f
seodowa <pagalan.theodoreustp@gmail.com>
2025-07-11 15:35:47 +0800

    Implement "computer" logic
    Implement computer logic for randomly choosing
    between rock, paper, or scissors.

--- logic.js
+++ logic.js
@@ -1,1 +1,13 @@
-console.log("Test");
+function getComputerChoice() {
+    let randomNum = Math.ceil(Math.random() * 3)
+    switch (randomNum) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
