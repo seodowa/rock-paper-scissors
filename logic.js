41c7a1d36e95ade49cb9f3b5ceb421e6fd9ee19a
seodowa <pagalan.theodoreustp@gmail.com>
2025-07-11 17:51:54 +0800

    Add functionality to evaluate a winner

--- logic.js
+++ logic.js
@@ -1,6 +1,9 @@
 let restartBtn = document.getElementById("restartBtn");
 let submitBtn = document.getElementById("submitBtn");
-let humanChoice = document.getElementById("options");
+let humanChoiceText = document.getElementById("options");
+let computerChoiceText = document.getElementById("computer-choice");
+let resultText = document.getElementById("round-winner");
+let roundNumber = document.getElementById("round-number");
 let humanScore = 0;
 let computerScore = 0;
 
@@ -11,17 +14,78 @@ submitBtn.addEventListener("click", () => {
     } else {
         restartBtn.style.visibility = "hidden";
     }
-    console.log(getHumanChoice());
+    playRound(getHumanChoice(), getComputerChoice());
 })
 
 
-function playRound() {
-    
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    computerChoiceText.textContent = toTitleCase(computerChoice);
+
+    switch (humanChoice) {
+        case "paper":
+            paperAgainst(computerChoice);
+            break;
+        case "rock":
+            rockAgainst(computerChoice);
+            break;
+        case "scissors":
+            scissorsAgainst(computerChoice);
+            break;
+    }
+
+}
+
+
+function paperAgainst(computerChoice) {
+    resultText.textContent.replaceAll("");
+    switch (computerChoice) {
+        case "paper":
+            resultText.textContent = "Draw!";
+            break;
+        case "rock":
+            resultText.textContent = `You win! Paper beats ${toTitleCase(computerChoice)}.`;
+            break;
+        case "scissors":
+            resultText.textContent = `You lose! ${toTitleCase(computerChoice)} beats Paper.`;
+            break;
+    }
+}
+
+
+function rockAgainst(computerChoice) {
+    switch (computerChoice) {
+        case "paper":
+            resultText.textContent = `You lose! ${toTitleCase(computerChoice)} beats Rock.`;
+            break;
+        case "rock":
+            resultText.textContent = "Draw!";
+            break;
+        case "scissors":
+            resultText.textContent = `You win! Rock beats ${toTitleCase(computerChoice)}.`;
+            break;
+    }
+}
+
+
+function scissorsAgainst(computerChoice) {
+    switch (computerChoice) {
+        case "paper":
+            resultText.textContent = `You win! Scissors beats ${toTitleCase(computerChoice)}.`;
+            break;
+        case "rock":
+            resultText.textContent = `You lose! ${toTitleCase(computerChoice)} beats Scissors.`;
+            break;
+        case "scissors":
+            resultText.textContent = "Draw!";
+            break;
+    }
 }
 
 
 function getHumanChoice() {
-    return humanChoice.value;
+    return humanChoiceText.value;
 }
 
 
@@ -29,10 +93,19 @@ function getComputerChoice() {
     let randomNum = Math.ceil(Math.random() * 3)
     switch (randomNum) {
         case 1:
-            return "Rock";
+            return "rock";
         case 2:
-            return "Paper";
+            return "paper";
         case 3:
-            return "Scissors";
+            return "scissors";
     }
+}
+
+
+// credits to geeksforgeeks
+function toTitleCase(s) {
+    return s.toLowerCase()
+            .split(' ')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(' ');
 }
